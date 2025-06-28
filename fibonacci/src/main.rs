a02113d75a6e98da1f8f132f5933f88366c7b378
charlycator <devcharly@pm.me>
2025-06-28 17:57:30 +0200

    Fibo wants to save the world

---
+++ fibonacci/src/main.rs
@@ -0,0 +1,31 @@
+use std::io;
+
+fn calc_fibo(num: i64, prev: i64, curr: i64) -> i64 {
+    println!("NUM: {num} -- prev: {prev} -- curr: {curr}");
+
+    if num == 0 {
+        prev
+    } else if num == 1 {
+        curr
+    } else {
+        calc_fibo(num - 1, curr, prev + curr)
+    }
+}
+
+fn main() {
+    let mut typed_number = String::new();
+
+    println!("Give me a number");
+
+    io::stdin()
+        .read_line(&mut typed_number)
+        .expect("Failed to read the number you typed");
+
+    let typed_number = typed_number
+        .trim()
+        .parse()
+        .expect("Type a number!!!");
+    let result: i64 = calc_fibo(typed_number, 0, 1);
+
+    println!("Fibo calculation is {result}");
+}
